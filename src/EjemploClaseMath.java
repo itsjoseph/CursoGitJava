93061bbbe08a5219bc530207fe0805d51fd55eee
conor <conor@localhost.localdomain>
2025-01-14 00:57:16 -0600

    Clase Math

---
+++ src/EjemploClaseMath.java
@@ -0,0 +1,19 @@
+public class EjemploClaseMath {
+
+    public static void main(String[] args) {
+        int enteroabs = Math.abs(-32);
+        System.out.println(enteroabs);
+        
+        double max = Math.max(3.5, 4.2);
+        System.out.println("max = " + max);
+
+        double min = Math.min(3.5, 4.2);
+        System.out.println("min = " + min);
+
+        double techo = Math.ceil(3.5);
+        System.out.println("techo = " + techo);
+
+        double floor = Math.floor(3.5);
+        System.out.println("floor = " + floor);
+    }
+}
---
+++ src/EjemploClaseMathRandom.java
@@ -0,0 +1,6 @@
+public class EjemploClaseMathRandom {
+    public static void main(String[] args) {
+        double random = Math.random();
+        System.out.println(Math.round(random * 100));
+    }
+}
